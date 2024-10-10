deb8678a24295e2d1f28373f0718f4e5254545d3
arjunchainani <arjun.k.chainani@gmail.com>
2024-10-10 00:05:32 -0500

    added initial oracle implementation

--- resspect/classifiers.py
+++ resspect/classifiers.py
@@ -25,6 +25,10 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.utils import resample
 from sklearn.utils.validation import check_is_fitted
 
+import os
+import networkx as nx
+import tf_keras
+
 __all__ = ['random_forest',#'gradient_boosted_trees',
            'knn',
            'mlp','svm','nbg', 'bootstrap_clf'
@@ -87,7 +91,7 @@ def bootstrap_clf(clf_function, n_ensembles, train_features,
 
 
 def random_forest(train_features:  np.array, train_labels: np.array,
-                  test_features: np.array, n_estimators=100, **kwargs):
+                  test_features: np.array, n_estimators=1000, retrain: bool = True, curr_clf=None, **kwargs):
     """Random Forest classifier.
 
     Parameters
@@ -113,7 +117,7 @@ def random_forest(train_features:  np.array, train_labels: np.array,
     """
 
     # create classifier instance
-    clf = RandomForestClassifier(n_estimators=n_estimators, **kwargs)
+    clf = RandomForestClassifier(n_estimators=n_estimators, **kwargs) if retrain == True else curr_clf
     clf.fit(train_features, train_labels)                     # train
     predictions = clf.predict(test_features)                # predict
     prob = clf.predict_proba(test_features)       # get probabilities
@@ -158,7 +162,7 @@ def random_forest(train_features:  np.array, train_labels: np.array,
 #########################################################################
 
 def knn(train_features: np.array, train_labels: np.array,
-        test_features: np.array, **kwargs):
+        test_features: np.array, retrain: bool = True, curr_clf=None, **kwargs):
 
     """K-Nearest Neighbour classifier.
 
@@ -182,7 +186,7 @@ def knn(train_features: np.array, train_labels: np.array,
     """
 
     #create classifier instance
-    clf = KNeighborsClassifier(**kwargs)
+    clf = KNeighborsClassifier(**kwargs) if retrain == True else curr_clf
 
     clf.fit(train_features, train_labels)              # train
     predictions = clf.predict(test_features)           # predict
@@ -192,7 +196,7 @@ def knn(train_features: np.array, train_labels: np.array,
 
 
 def mlp(train_features: np.array, train_labels: np.array,
-        test_features: np.array, **kwargs):
+        test_features: np.array, retrain: bool = True, curr_clf=None, **kwargs):
 
     """Multi Layer Perceptron classifier.
 
@@ -216,7 +220,7 @@ def mlp(train_features: np.array, train_labels: np.array,
     """
 
     #create classifier instance
-    clf=MLPClassifier(**kwargs)
+    clf = MLPClassifier(**kwargs) if retrain == True else curr_clf
 
     clf.fit(train_features, train_labels)              # train
     predictions = clf.predict(test_features)           # predict
@@ -225,7 +229,7 @@ def mlp(train_features: np.array, train_labels: np.array,
     return predictions, prob, clf
 
 def svm(train_features: np.array, train_labels: np.array,
-        test_features: np.array, **kwargs):
+        test_features: np.array, retrain: bool = True, curr_clf=None, **kwargs):
     """Support Vector classifier.
 
     Parameters
@@ -249,7 +253,7 @@ def svm(train_features: np.array, train_labels: np.array,
     """
 
     #create classifier instance
-    clf = SVC(probability=True, **kwargs)
+    clf = SVC(probability=True, **kwargs) if retrain == True else curr_clf
 
     clf.fit(train_features, train_labels)          # train
     predictions = clf.predict(test_features)       # predict
@@ -259,7 +263,7 @@ def svm(train_features: np.array, train_labels: np.array,
   
 
 def nbg(train_features: np.array, train_labels: np.array,
-                  test_features: np.array, **kwargs):
+                  test_features: np.array, retrain: bool = True, curr_clf=None, **kwargs):
 
     """Naive Bayes classifier.
 
@@ -284,7 +288,7 @@ def nbg(train_features: np.array, train_labels: np.array,
     """
 
     #create classifier instance
-    clf=GaussianNB(**kwargs)
+    clf=GaussianNB(**kwargs) if retrain == True else curr_clf
 
     clf.fit(train_features, train_labels)         # fit
     predictions = clf.predict(test_features)      # predict
@@ -382,6 +386,148 @@ class PreFitVotingClassifier(object):
     def _predict(self, X):
         """Collect results from clf.predict calls. """
         return np.asarray([clf.predict(X) for clf in self.estimators]).T
+
+class ORACLE:
+    def __init__(self):
+        os.environ["TF_USE_LEGACY_KERAS"] = 1
+        self.source_node_label = "Alert"
+        self.model = None
+        self.tree = None
+        self.y_pred = None
+        self.class_probabilities = None
+        self.class_conditional_probabilities = None
+        self.true_labels = []
+        self.pred_labels = []
+        
+        self.get_taxonomy_tree()
+    
+    def get_taxonomy_tree(self):
+        # Graph to store taxonomy
+        self.tree = nx.DiGraph(directed=True)
+
+        self.tree.add_node('Alert', color='red')
+
+        # Level 1
+        level_1_nodes = ['Transient', 'Variable']
+        self.tree.add_nodes_from(level_1_nodes)
+        self.tree.add_edges_from([('Alert', level_1_node) for level_1_node in level_1_nodes])
+
+        # Level 2a nodes for Transients
+        level_2a_nodes = ['SN', 'Fast', 'Long']
+        self.tree.add_nodes_from(level_2a_nodes)
+        self.tree.add_edges_from([('Transient', level_2a_node) for level_2a_node in level_2a_nodes])
+
+        # Level 2b nodes for Transients
+        level_2b_nodes = ['Periodic', 'AGN']
+        self.tree.add_nodes_from(level_2b_nodes)
+        self.tree.add_edges_from([('Variable', level_2b_node) for level_2b_node in level_2b_nodes])
+
+        # Level 3a nodes for SN Transients
+        level_3a_nodes = ['SNIa', 'SNIb/c', 'SNIax', 'SNI91bg', 'SNII']
+        self.tree.add_nodes_from(level_3a_nodes)
+        self.tree.add_edges_from([('SN', level_3a_node) for level_3a_node in level_3a_nodes])
+
+        # Level 3b nodes for Fast events Transients
+        level_3b_nodes = ['KN', 'Dwarf Novae', 'uLens', 'M-dwarf Flare']
+        self.tree.add_nodes_from(level_3b_nodes)
+        self.tree.add_edges_from([('Fast', level_3b_node) for level_3b_node in level_3b_nodes])
+
+        # Level 3c nodes for Long events Transients
+        level_3c_nodes = ['SLSN', 'TDE', 'ILOT', 'CART', 'PISN']
+        self.tree.add_nodes_from(level_3c_nodes)
+        self.tree.add_edges_from([('Long', level_3c_node) for level_3c_node in level_3c_nodes])
+
+        # Level 3d nodes for periodic stellar events
+        level_3d_nodes = ['Cepheid', 'RR Lyrae', 'Delta Scuti', 'EB'] 
+        self.tree.add_nodes_from(level_3d_nodes)
+        self.tree.add_edges_from([('Periodic', level_3d_node) for level_3d_node in level_3d_nodes])
+        
+        self.level_order_nodes = list(nx.bfs_tree(self.tree, source=self.source_node_label).nodes())
+
+    def get_predictions(self, data, path_to_weights=None):
+        if path_to_weights is not None:
+            self.model = tf_keras.models.load_model(path_to_weights, compile=False)
+        else:
+            pass # needs to be implemented at some point in the future
+        
+        self.y_pred = self.model.predict(data)
+    
+    def get_conditional_probabilities(self):
+        # Create a new arrays to store pseudo (conditional) probabilities.
+        self.class_conditional_probabilities = np.copy(self.y_pred)
+        self.class_probabilities = np.copy(self.y_pred)
+
+        parents = [list(self.tree.predecessors(node)) for node in self.level_order_nodes]
+        for idx in range(len(parents)):
+
+            # Make sure the graph is a tree.
+            assert len(parents[idx]) == 0 or len(parents[idx]) == 1, 'Number of parents for each node should be 0 (for root) or 1.'
+            
+            if len(parents[idx]) == 0:
+                parents[idx] = ''
+            else:
+                parents[idx] = parents[idx][0]
+
+        # Finding unique parents for masking
+        unique_parents = list(set(parents))
+        unique_parents.sort()
+
+        # Create masks for applying soft max and calculating loss values.
+        masks = []
+        for parent in unique_parents:
+            masks.append(self.get_indices_where(parents, parent))
+        
+        # Get the masked softmaxes
+        for mask in masks:
+            self.class_probabilities[:, mask] = np.exp(self.y_pred[:, mask]) / np.sum(np.exp(self.y_pred[:, mask]), axis=1, keepdims=True)
+            
+
+        for node in self.level_order_nodes:
+            
+            # Find path from node to 
+            path = list(nx.shortest_path(self.tree, self.source_node_label, node))
+            
+            # Get the index of the node for which we are calculating the pseudo probability
+            node_index = self.level_order_nodes.index(node)
+            
+            # Indices of all the classes in the path from source to the node for which we are calculating the pseudo probability
+            path_indices = [self.level_order_nodes.index(u) for u in path]
+            
+            #print(node, path, node_index, path_indices, pseudo_probabilities[:, path_indices])
+            
+            # Multiply the pseudo probabilites of all the classes in the path so that we get the conditional pseudo probabilites
+            self.class_conditional_probabilities[:, node_index] = np.prod(self.class_probabilities[:, path_indices], axis = 1)
+            
+    def get_indices_where(arr, target):
+        to_return = []
+        for i, obs in enumerate(arr):
+            if obs == target:
+                to_return.append(i)
+                
+        return to_return
+
+    def sort_nodes_and_generate_labels(self, label_pred, label_true):
+        depths = [len(nx.shortest_path(self.tree, source=self.source_node_label, target=node)) for node in self.level_order_nodes]
+        
+        masks = []
+        unique_depths = list(set(depths)).sort()
+        
+        for depth in unique_depths:
+            masks.append(self.get_indices_where(depths, depth))
+        
+        self.true_labels = []
+        self.pred_labels = []
+        
+        for mask, depth in zip(masks, unique_depths):
+            if depth != 0 and depth != 3:
+                mask_classes = [self.level_order_nodes[m] for m in mask]
+                
+                for i in range(label_true.shape[0]):
+                    true_class_idx = np.argmax(label_true[i, mask])
+                    self.true_labels.append(mask_classes[true_class_idx])
+                    
+                    pred_class_idx = np.argmax(label_pred[i, mask])
+                    self.pred_labels.append(mask_classes[pred_class_idx])
 
 def main():
     return None
